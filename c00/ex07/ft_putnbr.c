32b598c3040091d75830eb2699afd9c0b1100ad3
sergy <sergy.lopez.21@gmail.com>
2023-11-11 15:42:03 +0100

    falta limits

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= -1;
+	}
+	if (nb < 10)
+	{
+		c = (char) nb + '0';
+		write(1, &c, 1);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
+//int main(){
+//	ft_putnbr(868768);
+//}
