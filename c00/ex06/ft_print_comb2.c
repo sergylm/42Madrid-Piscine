1c900ef405e1a67f9e02364607dbacaf342a43b9
sergy <sergy.lopez.21@gmail.com>
2023-11-11 15:07:52 +0100

    ex06

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+
+void	ft_print(int i)
+{
+	char	ic1;
+	char	ic2;
+
+	if (i < 10)
+	{
+		ic1 = '0';
+		ic2 = (char) i + '0';
+	}
+	else
+	{
+		ic1 = (char) i / 10 + '0';
+		ic2 = (char) i % 10 + '0';
+	}
+	write(1, &ic1, 1);
+	write(1, &ic2, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			ft_print(i);
+			write(1, " ", 1);
+			ft_print(j);
+			if (i != 99 || j != 99)
+			{
+				write(1, ", ", 2);
+			}
+			j++;
+		}
+		i++;
+	}
+}
