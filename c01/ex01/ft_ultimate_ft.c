9dc8eb16979111ce504f9bf6db40527ef41c9c9e
sergy <sergy.lopez.21@gmail.com>
2023-11-11 22:28:39 +0100

    ex01

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+//
+//int main(){
+//	int a = 10;
+//	int *a1 = &a;
+//	int **a2 = &a1;
+//	int ***a3 = &a2;
+//	int ****a4 = &a3;
+//	int *****a5 = &a4;
+//	int ******a6 = &a5;
+//	int *******a7 = &a6;
+//	int ********a8 = &a7;
+//
+//	printf("%d\n",  ********a8);
+//	ft_ultimate_ft(&a8);
+//	printf("%d", ********a8);
+//}
