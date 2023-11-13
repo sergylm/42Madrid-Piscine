5e49bf0c207fa8ed3f4608b3a8396c48b608a5c7
sergy <sergy.lopez.21@gmail.com>
2023-11-13 13:55:57 +0100

    ex00

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -1,24 +1,25 @@
 //#include <stdio.h>
+//#include <string.h>
 char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
 
 	i = 0;
-	while (dest[i] != '\0' && src[i] != '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (dest[i] != '\0')
-		dest[i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
 //int main(){
-//	char str1[] = "test1", str2[] = "1234567890456789045678904567890", *result;
+//	char dest1[] = "1234567890", dest2[] = "1234567890";
+//	char  src[] = "0987654321aaa", *result_ft, *result_strcpy;
 //
-//	printf("dst:\"%s\"\nsrc:\"%s\"\n", str1, str2);
-//	result = ft_strcpy(str1, str2);
-//	printf("Copiado\n");
-//	printf("dst:\"%s\"\nsrc:\"%s\"\n", str1, str2);
-//	printf("resul:\"%s\"\n", result);
+//	printf("dst:\"%s\"\nsrc:\"%s\"\n", dest1, src);
+//	result_ft = ft_strcpy(dest1, src);
+//	printf("resul con ft_strcpy: \"%s\"\n", result_ft);
+//	result_strcpy = strcpy(dest2, src);
+//	printf("resul con strcpy:    \"%s\"\n", result_strcpy);
 //}
