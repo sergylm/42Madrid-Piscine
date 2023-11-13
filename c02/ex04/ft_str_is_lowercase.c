13029f6caf1335eaed970c86a20a1a6a37c5eb2a
sergy <sergy.lopez.21@gmail.com>
2023-11-13 14:50:46 +0100

    ex04

---
+++ c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+int	ft_str_is_lowercase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'a' || str[i] > 'z')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+//int main(){
+//	char *str = "holaaAAA";
+//	int result_ft = ft_str_is_lowercase(str);
+//
+//	printf("result con ft:  %d\n", result_ft);
+//
+//}
