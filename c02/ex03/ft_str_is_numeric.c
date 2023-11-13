c8f6f3ab88f1158e9199f721babfc93d3977d601
sergy <sergy.lopez.21@gmail.com>
2023-11-13 14:48:00 +0100

    ex03

---
+++ c02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+int	ft_str_is_numeric(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+//int main(){
+//	char *str = "1234";
+//	int result_ft = ft_str_is_numeric(str);
+//
+//	printf("result con ft:  %d\n", result_ft);
+//
+//}
