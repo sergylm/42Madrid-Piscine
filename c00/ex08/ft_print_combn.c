4221dab022424471baebea818466c3e3dd936bd7
sergy <sergy.lopez.21@gmail.com>
2023-11-14 17:38:46 +0100

    ex08

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -1,73 +1,95 @@
 #include <unistd.h>
 
-int	length(int n)
-{
-	int	len;
-
-	len = 1;
-	while (n > 10)
-	{
-		n /= 10;
-		len++;
-	}
-	return (len);
-}
-
-void	zeros(int n, int max)
+void	get_min_max(int n, int *min, int *max)
 {
 	int	i;
-	int	fill;
+	int	mul;
 
-	i = 0;
-	fill = length(max) - length(n);
-	while (i < fill)
+	i = n - 2;
+	mul = 1;
+	*max = 1;
+	*min = 1;
+	while (i-- + 2)
+		*max *= 10;
+	*max -= 1;
+	i = n - 1;
+	while (i-- + 1)
 	{
-		write(1, "0", 1);
-		i++;
+		*max = *max - ((n - i - 2) * mul);
+		mul *= 10;
 	}
+	i = n - 1;
+	while (i-- - 1)
+		*min = *min * 10 + (n - i);
 }
 
-void	ft_print(int n)
+void	zeros(int i, int n, int *zero)
 {
-	char	c;
+	int	mul;
 
-	if (n < 10)
+	mul = 1;
+	while (n-- - 1)
+		mul *= 10;
+	if (i < mul)
+		write(1, "0", 1);
+	else
+		*zero = 0;
+}
+
+int	check_number(int i)
+{
+	int	finish;
+	int	tmp;
+
+	tmp = i;
+	finish = 1;
+	while (tmp / 10 != 0 && finish)
 	{
-		c = (char) n + '0';
-		write(1, &c, 1);
+		if (tmp % 10 <= (tmp / 10) % 10)
+			finish = 0;
+		tmp /= 10;
+	}
+	return (finish);
+}
+
+void	print(int i)
+{
+	char	ic;
+
+	if (i < 10)
+	{
+		ic = (char) i + '0';
+		write(1, &ic, 1);
 	}
 	else
 	{
-		ft_print(n / 10);
-		ft_print(n % 10);
+		print(i / 10);
+		print(i % 10);
 	}
 }
 
 void	ft_print_combn(int n)
 {
-	int	i;
+	int	min;
 	int	max;
+	int	zero;
 
-	i = 1;
-	max = 10;
-	while (i < n)
+	zero = 1;
+	get_min_max(n, &min, &max);
+	while (min < max)
 	{
-		max *= 10;
-		i++;
-	}
-	i = 0;
-	while (i < max)
-	{
-		zeros(i, max -1);
-		ft_print(i);
-		if (i != max - 1)
+		if (check_number(min))
 		{
+			if (zero)
+				zeros(min, n, &zero);
+			print(min);
 			write(1, ", ", 2);
 		}
-		i++;
+		min++;
 	}
+	print(max);
 }
 //int	main(void)
 //{
-//	ft_print_combn(4);
+//	ft_print_combn(2);
 //}
