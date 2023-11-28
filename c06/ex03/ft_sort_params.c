4ceaf541da8f9ca297f42f62acf71440f8add26f
sergylopez21 <47850202+sergylopez21@users.noreply.github.com>
2023-11-28 19:33:54 +0100

    c06

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: selopez- <selopez-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 20:29:31 by selopez-          #+#    #+#             */
+/*   Updated: 2023/11/28 12:58:11 by selopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	swap(int argc, char *argv[])
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	while (i < argc)
+	{
+		j = 1;
+		while (j < (argc - i - 1))
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				temp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	swap(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		while (*argv[i])
+		{
+			write(1, argv[i], 1);
+			argv[i]++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
